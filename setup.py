5dbb7a3463b259e5336fa324d69ae5ba5d25da4b
Daniel Bartolini <djb3149@gmail.com>
2024-05-07 10:43:05 -0400

    setup command

--- setup.py
+++ setup.py
@@ -8,9 +8,27 @@ print("Compiling project . . . . . .")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 # Compile command
-compile_command = os.system("clang++ -o sudoku-generator.exe main.cpp generator.cpp")
+compile_line = os.system("clang++ -o sudoku-generator.exe main.cpp generator.cpp")
 
-if compile_command == 0:
+# Compile flag
+if compile_line == 0:
     print("~~~~~~~~~~~~~~~~~~~~~")
     print("Compiled successfully")
     print("~~~~~~~~~~~~~~~~~~~~~")
+
+    # Determining platform
+    namestr = platform.system()
+
+    if namestr == 'Linux':
+        platform = 'linux'
+
+    elif namestr == 'Darwin':
+        platform = 'macos'
+
+    # Command alias
+    alias_line = f"alias generate='{os.path.abspath('sudoku-generator.exe')}'"
+
+    shell_profile = os.path.expanduser("~/.bashrc" if platform == 'linux' else "~/.bash_profile")
+    if not any("sudoku-generator" in line for line in open(shell_profile)):
+        with open(shell_profile, "a") as profile:
+            profile.write(alias_line + "\n")
